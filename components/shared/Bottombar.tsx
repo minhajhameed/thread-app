1a1bce8408092861eef74163c08abe580596afbd
MinhajHameed <minhajham@yahoo.com>
2024-01-02 22:27:00 +0530

    side comp

---
+++ components/shared/Bottombar.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import { sidebarLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+function bottombar() {
+
+    const pathname = usePathname();
+
+    return (
+        <section className="bottombar">
+          <div className="bottombar_container">
+          {sidebarLinks.map((link) => {
+                    const isActive = (pathname.includes(link.route) && link.route.length > 1) || pathname === link.route
+
+                    return (
+                    <Link 
+                    href={link.route}
+                    key={link.label}
+                    className={`leftsidebar_link ${isActive ? 'bg-violet-500' : ''}`}
+                    >
+                        <Image
+                        src={link.imgURL}
+                        alt={link.label}
+                        width={24}
+                        height={24} 
+                        />
+
+                        <p className="text-light-1 max-lg:hidden">
+                            {link.label}
+                        </p>
+                    </Link>
+                )}
+                )}
+          </div>
+        </section>
+    )
+}
+
+export default bottombar;
